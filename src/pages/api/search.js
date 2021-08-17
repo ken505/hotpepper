24489ed577f88501d25be7df53b23eae0b15e178
ken505 <68226398+ken505@users.noreply.github.com>
2021-08-17 17:24:26 +0900

    ✨

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
-// const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&large_area=Z011`
-const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&large_service_area=SS20`
+const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&large_area=Z011`
+// const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&large_service_area=SS20`
 
 export default async (req, res) => {
   let url = defaultEndpoint
